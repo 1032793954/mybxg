7e8123e2a5b4144ccf605f1be56b6378a0becc48
苏佳佳 <1032793954@qq.com>
2017-08-30 20:44:16 +0800

    完成退出和登陆功能

--- pubilc/js/common.js
+++ pubilc/js/common.js
@@ -6,3 +6,22 @@
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+
+	/*实现退出功能点击退出按钮删除cookieid*/
+	$('#logoutBtn').click(function(){
+		/*发送ajax请求*/
+		$.ajax({
+			/*请求接口*/
+			url:'/api/logout',
+			/*请求方式*/
+			type:'post',
+			dataType:'json',
+			/*请求成功之后判断有没有cookieid如果有就删除跳转到登录页*/
+			success:function(data){
+				if(data.code==200){
+					/*跳转到登录页*/
+					location.href='/main/login'
+				}
+			}
+		})
+	})
